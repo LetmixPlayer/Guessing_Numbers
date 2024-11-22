6de4c7072ac429b6008f8bfa01cb2ef13c59b28d
Letmix <147415674+LetmixPlayer@users.noreply.github.com>
2024-11-22 20:58:12 +0800

    Uploaded files

---
+++ log.py
@@ -0,0 +1,20 @@
+import os
+import sys
+def init():
+    a = open("log.txt", "w")
+    a.close()
+
+def log(message : str):
+    a = open("log.txt", 'a')
+    a.write('\n' + message)
+    a.close()
+    try:
+        if (sys.argv[2] == "DebLet"): print(message)
+    except:
+        pass
+
+def clear():
+    try:
+        os.remove("log.txt")
+    except:
+        pass
